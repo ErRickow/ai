4cf5db13cab409a4175929b2efe1ef3100c7ad27
Just R <remixingmagelang@gmail.com>
2025-05-30 01:16:37 +0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,14 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/index.ts'
+  ],
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
-      branches: 80,
+      branches: 70,
       functions: 80,
       lines: 80,
       statements: 80
